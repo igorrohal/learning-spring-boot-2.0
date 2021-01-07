c1c89306d85009f448660dca0db92e1619114c9f
Igor Rohal <igor.rohal@gmail.com>
2021-01-07 23:34:58 +0100

    testing integration with Heroku

--- src/main/java/eu/irohal/springbootreactive/SpringBootReactiveApplication.java
+++ src/main/java/eu/irohal/springbootreactive/SpringBootReactiveApplication.java
@@ -3,6 +3,9 @@ package eu.irohal.springbootreactive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Application class
+ */
 @SpringBootApplication
 public class SpringBootReactiveApplication {
 
